0315a8e4de3ffe827c9f9d0934e93e363b119cd2
azizbeknajimov3-jpg <azizbeknajimov3@gmail.com>
2025-08-19 07:27:49 +0500

    Update User.js

--- modelda/User.js
+++ modelda/User.js
@@ -29,3 +29,15 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("User", userSchema);
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+
+  balanceCct: { type: Number, default: 0 },   // foydalanuvchi CCT balansi
+  balanceFiat: { type: Number, default: 0 },  // real pul balansi (USD, UZS, va hokazo)
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", UserSchema)
