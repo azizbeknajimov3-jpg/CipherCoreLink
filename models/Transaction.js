f29c57a9d4d98987f1ad244d1ebcd559232afb88
azizbeknajimov3-jpg <azizbeknajimov3@gmail.com>
2025-08-19 21:17:38 +0500

    Create Transaction.js

---
+++ models/Transaction.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  type: { type: String, enum: ["deposit", "withdraw", "revenue"], required: true },
+  amount: { type: Number, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Transaction", transactionSchema);
